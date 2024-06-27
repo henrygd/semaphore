7dbb2d59b6fb08754b0d7e84ec7f2865df4e2ad3
Henry Dollman <hank@henrygd.me>
2024-06-27 17:59:21 -0400

    tests

---
+++ test/bun.test.ts
@@ -0,0 +1,143 @@
+import { test, expect } from 'bun:test'
+import { env } from 'bun'
+import { getSemaphore as getSemaphoreDev } from '../index.ts'
+import { getSemaphore as getSemaphoreDist } from '../dist/index.js'
+
+let getSemaphore: typeof getSemaphoreDev
+if (env.DIST) {
+	console.log('using dist files')
+	getSemaphore = getSemaphoreDist
+} else {
+	console.log('using dev files')
+	getSemaphore = getSemaphoreDev
+}
+
+const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+
+test('Simple acquire and release', async () => {
+	const sem = getSemaphore()
+	expect(sem.size()).toBe(0)
+	await sem.acquire()
+	expect(sem.size()).toBe(1)
+	sem.release()
+	expect(sem.size()).toBe(0)
+	await sem.acquire()
+	expect(sem.size()).toBe(1)
+	sem.release()
+	expect(sem.size()).toBe(0)
+})
+
+test('Releases properly after task', async () => {
+	const sem = getSemaphore()
+	expect(sem.size()).toBe(0)
+	sem
+		.acquire()
+		.then(async () => await wait(100))
+		.finally(() => sem.release())
+	expect(sem.size()).toBe(1)
+	await wait(50)
+	expect(sem.size()).toBe(1)
+	await wait(60)
+	expect(sem.size()).toBe(0)
+})
+
+test('Staggered locks', async () => {
+	const sem = getSemaphore('testing', 2)
+	let val = 0
+
+	sem
+		.acquire()
+		.then(() => wait(100))
+		.then(() => (val = 20))
+		.finally(sem.release)
+
+	sem
+		.acquire()
+		.then(() => wait(50))
+		.then(() => (val = 10))
+		.finally(sem.release)
+
+	expect(sem.size()).toBe(2)
+	expect(val).toBe(0)
+
+	await sem
+		.acquire()
+		.then(() => expect(val).toBe(10))
+		.finally(sem.release)
+
+	expect(sem.size()).toBe(1)
+	// wait for first lock to complete
+	await wait(110)
+	expect(sem.size()).toBe(0)
+})
+
+test('Can reuse same active sem with key', async () => {
+	for (let i = 0; i < 5; i++) {
+		;(async () => {
+			const sem = getSemaphore('reuse-test')
+			await sem.acquire()
+			await wait(10)
+			sem.release()
+		})()
+	}
+	await wait(5)
+	const sem = getSemaphore('reuse-test')
+	for (let i = 5; i >= 0; i--) {
+		expect(sem.size()).toBe(i)
+		await wait(10)
+	}
+})
+
+test('Cannot reuse sem w/o key', async () => {
+	for (let i = 0; i < 5; i++) {
+		;(async () => {
+			const sem = getSemaphore()
+			await sem.acquire()
+			await wait(10)
+			sem.release()
+		})()
+	}
+	await wait(5)
+	const sem = getSemaphore()
+	for (let i = 5; i >= 0; i--) {
+		expect(sem.size()).toBe(0)
+		await wait(10)
+	}
+})
+
+test('Concurrency works', async () => {
+	const sem = getSemaphore('concurrency-test', 5)
+	for (let i = 0; i < 50; i++) {
+		;(async () => {
+			await sem.acquire()
+			await wait(10)
+			sem.release()
+		})()
+	}
+	await wait(5)
+	for (let i = 50; i >= 0; i -= 5) {
+		expect(sem.size()).toBe(i)
+		await wait(10)
+	}
+})
+
+test("Won't explode if release is called too many times", async () => {
+	const semaphore = getSemaphore('testing', 2)
+	expect(semaphore.size()).toBe(0)
+	semaphore.release()
+	expect(semaphore.size()).toBe(0)
+	await semaphore.acquire()
+	expect(semaphore.size()).toBe(1)
+	semaphore.release()
+	semaphore.release()
+	semaphore.release()
+	expect(semaphore.size()).toBe(0)
+	await semaphore.acquire()
+	expect(semaphore.size()).toBe(1)
+	await semaphore.acquire()
+	expect(semaphore.size()).toBe(2)
+	semaphore.release()
+	semaphore.release()
+	semaphore.release()
+	expect(semaphore.size()).toBe(0)
+})
